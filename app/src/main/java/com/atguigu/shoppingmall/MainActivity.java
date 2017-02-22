d45ede21769910f07f98bdd1b779b75f259e1918
xm890419 <506083998@qq.com>
2017-02-22 15:06:28 +0800

    主页面布局文件,使用Butterknife初始化布局

--- app/src/main/java/com/atguigu/shoppingmall/MainActivity.java
+++ app/src/main/java/com/atguigu/shoppingmall/MainActivity.java
@@ -1,13 +1,26 @@
 package com.atguigu.shoppingmall;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fl_main)
+    FrameLayout flMain;
+    @BindView(R.id.rg_main)
+    RadioGroup rgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        rgMain.check(R.id.rb_home);
     }
 }
