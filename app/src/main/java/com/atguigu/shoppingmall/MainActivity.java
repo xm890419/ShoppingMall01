4f734154fa6ad69e9574739287f31ac5173f5bdf
xm890419 <506083998@qq.com>
2017-02-22 14:30:53 +0800

    启动页面

--- app/src/main/java/com/atguigu/shoppingmall/MainActivity.java
+++ app/src/main/java/com/atguigu/shoppingmall/MainActivity.java
@@ -3,11 +3,11 @@ package com.atguigu.shoppingmall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Welcome extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/atguigu/shoppingmall/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.atguigu.shoppingmall;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        },2000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            startMainActivity();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+}
