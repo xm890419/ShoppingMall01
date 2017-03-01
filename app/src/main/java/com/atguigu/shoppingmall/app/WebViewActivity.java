5533d0ad2133b5d7e3f825a050ebe5e777edaf26
xm890419 <506083998@qq.com>
2017-03-01 16:20:51 +0800

    联系客服CallCenterActivity  活动页面H5页面添加数据到购物车

--- app/src/main/java/com/atguigu/shoppingmall/app/WebViewActivity.java
+++ app/src/main/java/com/atguigu/shoppingmall/app/WebViewActivity.java
@@ -1,8 +1,11 @@
 package com.atguigu.shoppingmall.app;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceRequest;
@@ -15,14 +18,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.shoppingmall.R;
 import com.atguigu.shoppingmall.home.adapter.HomeAdapter;
+import com.atguigu.shoppingmall.home.bean.GoodsBean;
+import com.atguigu.shoppingmall.home.bean.H5Bean;
 import com.atguigu.shoppingmall.home.bean.WebViewBean;
 import com.atguigu.shoppingmall.utils.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.atguigu.shoppingmall.home.adapter.HomeAdapter.GOODS_BEAN;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -57,7 +65,7 @@ public class WebViewActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(true);
         webSettings.setUseWideViewPort(true);
-        webview.setWebViewClient(new WebViewClient(){
+        webview.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
@@ -79,13 +87,27 @@ public class WebViewActivity extends AppCompatActivity {
             }
         });
         //1.添加addJavascriptInterface(JAVA与H5互调)
-        webview.addJavascriptInterface(new MyJavascriptInterface(),"cyc");
-        webview.loadUrl(Constants.BASE_URL_IMAGE+webViewBean.getUrl());
+        webview.addJavascriptInterface(new MyJavascriptInterface(), "cyc");
+        webview.loadUrl(Constants.BASE_URL_IMAGE + webViewBean.getUrl());
     }
-    class MyJavascriptInterface{
+
+    class MyJavascriptInterface {
         @JavascriptInterface
-        public void jumpForAndroid(String data){
-            Toast.makeText(WebViewActivity.this, "data=="+data, Toast.LENGTH_SHORT).show();
+        public void jumpForAndroid(String data) {
+            Log.e("TAG", "" + data);
+            //Toast.makeText(WebViewActivity.this, "data=="+data, Toast.LENGTH_SHORT).show();
+            if (!TextUtils.isEmpty(data)) {
+                H5Bean h5Bean = JSON.parseObject(data, H5Bean.class);
+                //创建商品信息Bean对象
+                GoodsBean goodsBean = new GoodsBean();
+                goodsBean.setProduct_id(h5Bean.getValue().getProduct_id() + "");
+                goodsBean.setCover_price("10080");
+                goodsBean.setFigure("/1478770583834.png");
+                goodsBean.setName("尚硅谷Android");
+                Intent intent = new Intent(WebViewActivity.this,GoodsInfoActivity.class);
+                intent.putExtra(GOODS_BEAN,goodsBean);
+                startActivity(intent);
+            }
         }
     }
 
