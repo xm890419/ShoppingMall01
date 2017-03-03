6a8f3f1f5f664a25b39743be23b0b5589d3c7f4c
xm890419 <506083998@qq.com>
2017-03-03 14:57:57 +0800

    使用fastjson解析数据

--- app/src/main/java/com/atguigu/shoppingmall/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall/type/fragment/ListFragment.java
@@ -1,16 +1,24 @@
 package com.atguigu.shoppingmall.type.fragment;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.shoppingmall.R;
 import com.atguigu.shoppingmall.base.BaseFragment;
 import com.atguigu.shoppingmall.type.adapter.TypeLeftAdapter;
+import com.atguigu.shoppingmall.type.bean.TypeBean;
+import com.atguigu.shoppingmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by 熊猛 on 2017/3/3.
@@ -26,6 +34,11 @@ public class ListFragment extends BaseFragment {
     private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
             "办公文具", "数码周边", "游戏专区"};
 
+    //联网的url的集合
+    private String[] urls = new String[]{Constants.SKIRT_URL, Constants.JACKET_URL, Constants.PANTS_URL, Constants.OVERCOAT_URL,
+            Constants.ACCESSORY_URL, Constants.BAG_URL, Constants.DRESS_UP_URL, Constants.HOME_PRODUCTS_URL, Constants.STATIONERY_URL,
+            Constants.DIGIT_URL, Constants.GAME_URL};
+
     @Override
     public View initView() {
         View view = View.inflate(mContext, R.layout.fragment_list, null);
@@ -37,7 +50,7 @@ public class ListFragment extends BaseFragment {
     public void initData() {
         super.initData();
         //textView.setText("ListFragment");
-        final TypeLeftAdapter leftAdapter = new TypeLeftAdapter(mContext,titles);
+        final TypeLeftAdapter leftAdapter = new TypeLeftAdapter(mContext, titles);
         lvLeft.setAdapter(leftAdapter);
         //设置itme的点击事件
         lvLeft.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -45,9 +58,32 @@ public class ListFragment extends BaseFragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //1.传入被点击的位置
                 leftAdapter.changeSelected(position);
+                //2.刷新
                 leftAdapter.notifyDataSetChanged();
             }
         });
+        //联网请求
+        getDataFromNet(urls[0]);
+    }
+
+    private void getDataFromNet(String url) {
+        OkHttpUtils.get().url(url).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                Log.e("TAG", "联网失败=="+e.getMessage());
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                Log.e("TAG", "联网成功==");
+                processData(response);
+            }
+        });
+    }
+
+    private void processData(String response) {
+        TypeBean typeBean = JSON.parseObject(response,TypeBean.class);
+        Toast.makeText(mContext, ""+typeBean.getResult().get(0).getName(), Toast.LENGTH_SHORT).show();
     }
 
 }
