51e42b34c8a9bb4b3523d81be3c7cbfaa648adbf
xm890419 <506083998@qq.com>
2017-03-04 13:21:06 +0800

    联网请求网络且解析数据

--- app/src/main/java/com/atguigu/shoppingmall/community/fragment/NewPostFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall/community/fragment/NewPostFragment.java
@@ -1,31 +1,63 @@
 package com.atguigu.shoppingmall.community.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.shoppingmall.R;
 import com.atguigu.shoppingmall.base.BaseFragment;
+import com.atguigu.shoppingmall.community.bean.NewPostBean;
+import com.atguigu.shoppingmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by 熊猛 on 2017/3/4.
  */
 
 public class NewPostFragment extends BaseFragment {
-    private TextView textView;
+
+    @BindView(R.id.lv_new_post)
+    ListView lvNewPost;
+
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_news_post, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("NewPostFragment");
+        getDataFromNet();
     }
+
+    private void getDataFromNet() {
+        OkHttpUtils.get().url(Constants.NEW_POST_URL).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                Log.e("TAG", "联网失败=="+e.getMessage());
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                Log.e("TAG", "联网成功==");
+                processData(response);
+            }
+        });
+    }
+
+    private void processData(String response) {
+        NewPostBean bean = JSON.parseObject(response,NewPostBean.class);
+        Toast.makeText(mContext, ""+bean.getResult().get(0).getSaying(), Toast.LENGTH_SHORT).show();
+
+    }
+
 }
